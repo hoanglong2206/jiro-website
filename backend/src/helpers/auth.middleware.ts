012017a03767be1908e32091321da1b700c70085
hoanglong2206 <21522307@gm.uit.edu.vn>
2025-12-17 13:17:01 +0700

    feat: add JWT verification and validation middleware

---
+++ backend/src/helpers/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { config } from "../config";
+import { IAuthPayload } from "../types/auth.interface";
+
+export function verifyJWT(req: Request, res: Response, next: NextFunction) {
+	try {
+		const auth = req.headers.authorization;
+		if (!auth || !auth.startsWith("Bearer ")) {
+			return res.status(401).json({ message: "Unauthorized" });
+		}
+		const token = auth.substring(7);
+		const payload = jwt.verify(
+			token,
+			config.JWT_SECRET as string,
+		) as IAuthPayload;
+		req.currentUser = payload;
+		return next();
+	} catch (error) {
+		return res.status(401).json({ message: "Unauthorized" });
+	}
+}
