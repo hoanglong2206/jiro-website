4f3c29cb0e8cb968b24b0af318abb7b92588dbbd
hoanglong2206 <21522307@gm.uit.edu.vn>
2025-12-17 12:57:03 +0700

    feat: add database configuration and connection logic (BE)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
+import { dbConnection } from "./database";
 import { App } from "./app";
 
 class Server {
 	public start(): void {
 		const app: Express = express();
 		const application: App = new App(app);
+		dbConnection();
 		application.start();
 	}
 }
