d7a92dec790ccfadc2f9359f2a57f65431af60fb
Hoang Long <thlong2206@gmail.com>
2026-01-14 11:18:45 +0700

    refactor: simplify StoreProvider by removing ProtectedProvider and direct children rendering

--- frontend/components/providers/StoreProvider.tsx
+++ frontend/components/providers/StoreProvider.tsx
@@ -4,24 +4,15 @@ import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { persister, store } from "@/store/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { useCheckAuthenticated } from "@/hooks/useCheckAuthenticated";
 
 interface StoreProviderProps {
 	children: ReactNode;
 }
 
-function ProtectedProvider({ children }: StoreProviderProps) {
-	useCheckAuthenticated();
-
-	return children;
-}
-
 export function StoreProvider({ children }: StoreProviderProps) {
 	return (
 		<Provider store={store}>
-			<PersistGate persistor={persister}>
-				<ProtectedProvider>{children}</ProtectedProvider>
-			</PersistGate>
+			<PersistGate persistor={persister}>{children}</PersistGate>
 		</Provider>
 	);
 }
