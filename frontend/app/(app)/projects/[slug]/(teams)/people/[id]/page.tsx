945a2bc0a5fe7fdda12b99eba7c3577c848e3d62
Hoang Long <thlong2206@gmail.com>
2026-01-15 13:18:32 +0700

    feat: add new pages for Person, Team, Chat, Home, Inbox, Projects, Notifications, and Profile

---
+++ frontend/app/(app)/projects/[slug]/(teams)/people/[id]/page.tsx
@@ -0,0 +1,5 @@
+const PersonPage = () => {
+	return <div>page</div>;
+};
+
+export default PersonPage;
---
+++ frontend/app/(app)/projects/[slug]/(teams)/teams/[id]/page.tsx
@@ -0,0 +1,5 @@
+const TeamPage = () => {
+	return <div>page</div>;
+};
+
+export default TeamPage;
